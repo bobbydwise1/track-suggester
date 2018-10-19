e86f1890eee916158cd54ce32223dfa339e49fef
Robert Lee <bobbydwise1@hotmail.com>
2018-10-19 12:36:50 -0700

    Add debug code to javascript

--- js/scripts.js
+++ js/scripts.js
@@ -9,17 +9,20 @@ $(document).ready(function() { /* This is the start of the wrapper to enable jav
   var answerQuestion5 = $("input:radio[name=quiz-q5]:checked").val();
 
   var winnerAnswer1 = 0;
-  var winnerAnswer1 = 0;
-  var winnerAnswer1 = 0;
-  var winnerAnswer1 = 0;
-  var winnerAnswer1 = 0;
+  var winnerAnswer2 = 0;
+  var winnerAnswer3 = 0;
+  var winnerAnswer4 = 0;
+  var winnerAnswer5 = 0;
 
 /* The next section of code is to determine who is the "winner" of the quiz. This is done without looping or arrays.  Ties will count.*/
 
-  if (answerQuestion1 === "answer1") {}
+  if (answerQuestion1 === "answer1") {winnerAnswer1 +=}
+  else if (answerQuestion 1 === "answer2") {winnerAnswer2 +=}
+  else if (answerQuestion 1 === "answer2") {winnerAnswer3 +=}
+  else if (answerQuestion 1 === "answer2") {winnerAnswer4 +=}
+  else {winnerAnswer5 +=}
 
-
-
+alert winnerQuestion1;  /*the branch logic and display logic appears to not work.....*/
 
 
 
