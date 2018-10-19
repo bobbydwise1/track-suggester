79aae60e3463a1c3dcd40f93d5b7f5186d00cfb9
Robert Lee <bobbydwise1@hotmail.com>
2018-10-19 13:09:53 -0700

    Add additional logic to final answer determination

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,58 @@
 $(document).ready(function() { /* This is the start of the wrapper to enable javascript to run at HTML document ready */
 
 /* The next section of code is to capture user input from the radio buttons on the index.html page. */
+  $("form#quiz-form-section").submit(function(event) {
+    var answerQuestion1 = $("input:radio[name=quiz-q1]:checked").val();
+    var answerQuestion2 = $("input:radio[name=quiz-q2]:checked").val();
+    var answerQuestion3 = $("input:radio[name=quiz-q3]:checked").val();
+    var answerQuestion4 = $("input:radio[name=quiz-q4]:checked").val();
+    var answerQuestion5 = $("input:radio[name=quiz-q5]:checked").val();
 
-  var answerQuestion1 = $("input:radio[name=quiz-q1]:checked").val();
-  var answerQuestion2 = $("input:radio[name=quiz-q2]:checked").val();
-  var answerQuestion3 = $("input:radio[name=quiz-q3]:checked").val();
-  var answerQuestion4 = $("input:radio[name=quiz-q4]:checked").val();
-  var answerQuestion5 = $("input:radio[name=quiz-q5]:checked").val();
+    alert(answerQuestion1, answerQuestion2, answerQuestion3, answerQuestion4, answerQuestion5);  /*debug line.....*/
 
-  var winnerAnswer1 = 0;
-  var winnerAnswer2 = 0;
-  var winnerAnswer3 = 0;
-  var winnerAnswer4 = 0;
-  var winnerAnswer5 = 0;
+      var winnerAnswer1 = 0;
+      var winnerAnswer2 = 0;
+      var winnerAnswer3 = 0;
+      var winnerAnswer4 = 0;
+      var winnerAnswer5 = 0;
 
-/* The next section of code is to determine who is the "winner" of the quiz. This is done without looping or arrays.  Ties will count.*/
+    /* The next section of code is to determine who is the "winner" of the quiz. This is done without looping or arrays.  Ties will count.*/
 
-  if (answerQuestion1 === "answer1") {winnerAnswer1 += 1}
-  else if (answerQuestion 1 === "answer2") {winnerAnswer2 += 1}
-  else if (answerQuestion 1 === "answer2") {winnerAnswer3 +=1 }
-  else if (answerQuestion 1 === "answer2") {winnerAnswer4 += 1}
-  else {winnerAnswer5 += 1}
+      if (answerQuestion1 === "answer1") {winnerAnswer1 += 1}
+      else if (answerQuestion1 === "answer2") {winnerAnswer2 += 1}
+      else if (answerQuestion1 === "answer3") {winnerAnswer3 += 1}
+      else if (answerQuestion1 === "answer4") {winnerAnswer4 += 1}
+                                         else {winnerAnswer5 += 1}
 
-alert( )winnerQuestion1);  /*the branch logic and display logic appears to not work.....*/
+      if (answerQuestion2 === "answer1") {winnerAnswer1 += 1}
+      else if (answerQuestion2 === "answer2") {winnerAnswer2 += 1}
+      else if (answerQuestion2 === "answer3") {winnerAnswer3 += 1}
+      else if (answerQuestion2 === "answer4") {winnerAnswer4 += 1}
+      else {winnerAnswer5 += 1}
+
+      if (answerQuestion3 === "answer1") {winnerAnswer1 += 1}
+      else if (answerQuestion3 === "answer2") {winnerAnswer2 += 1}
+      else if (answerQuestion3 === "answer3") {winnerAnswer3 += 1}
+      else if (answerQuestion3 === "answer4") {winnerAnswer4 += 1}
+      else {winnerAnswer5 += 1}
+
+      if (answerQuestion4 === "answer1") {winnerAnswer1 += 1}
+      else if (answerQuestion4 === "answer2") {winnerAnswer2 += 1}
+      else if (answerQuestion4 === "answer3") {winnerAnswer3 += 1}
+      else if (answerQuestion4 === "answer4") {winnerAnswer4 += 1}
+      else {winnerAnswer5 += 1}
+
+      if (answerQuestion5 === "answer1") {winnerAnswer1 += 1}
+      else if (answerQuestion5 === "answer2") {winnerAnswer2 += 1}
+      else if (answerQuestion5 === "answer3") {winnerAnswer3 += 1}
+      else if (answerQuestion5 === "answer4") {winnerAnswer4 += 1}
+      else {winnerAnswer5 += 1}
+
+    alert(answerQuestion1, answerQuestion2, answerQuestion3, answerQuestion4, answerQuestion5);  /*debug line.....*/
 
 
 
 
-
+  });  /* This closes the form button submit logic */
 
 });  /* This is the end of the wrapper to enable javascript to run at HTML document ready */
