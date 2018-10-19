ae6e254600f5dfa4549a89cf7d156bc3932f03b5
Robert Lee <bobbydwise1@hotmail.com>
2018-10-19 10:44:24 -0700

    Add corrections to answer id at index.html and scritps file extension name

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+/* This is the start of the wrapper to enable javascript to run at HTML document ready */
+$(document).ready(function() {
+
+
+/* The next section of code is to capture user input from the radio buttons on the index.html page. */
+
+  var winnerQuestion1 = $("input:radio[name=quiz-q1]:checked").val();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+/* This is the end of the wrapper to enable javascript to run at HTML document ready */
