81239e00050e85dd8636f1d0d2fd06f316228242
Robert Lee <bobbydwise1@hotmail.com>
2018-10-19 11:01:39 -0700

    Add corrections to user input scripts and related html tags

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,22 @@
-/* This is the start of the wrapper to enable javascript to run at HTML document ready */
-$(document).ready(function() {
-
+$(document).ready(function() { /* This is the start of the wrapper to enable javascript to run at HTML document ready */
 
 /* The next section of code is to capture user input from the radio buttons on the index.html page. */
 
-  var winnerQuestion1 = $("input:radio[name=quiz-q1]:checked").val();
+  var answerQuestion1 = $("input:radio[name=quiz-q1]:checked").val();
+  var answerQuestion2 = $("input:radio[name=quiz-q2]:checked").val();
+  var answerQuestion3 = $("input:radio[name=quiz-q3]:checked").val();
+  var answerQuestion4 = $("input:radio[name=quiz-q4]:checked").val();
+  var answerQuestion5 = $("input:radio[name=quiz-q5]:checked").val();
+
+  var winnerAnswer1 = 0;
+  var winnerAnswer1 = 0;
+  var winnerAnswer1 = 0;
+  var winnerAnswer1 = 0;
+  var winnerAnswer1 = 0;
+
+/* The next section of code is to determine who is the "winner" of the quiz. This is done without looping or arrays.  Ties will count.*/
+
+  if (answerQuestion1 === "answer1") {}
 
 
 
@@ -14,13 +26,4 @@ $(document).ready(function() {
 
 
 
-
-
-
-
-
-
-
-
-});
-/* This is the end of the wrapper to enable javascript to run at HTML document ready */
+});  /* This is the end of the wrapper to enable javascript to run at HTML document ready */
