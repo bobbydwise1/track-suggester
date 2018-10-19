9395ff72372cbde2051e0340099b8dc688547f12
Robert Lee <bobbydwise1@hotmail.com>
2018-10-19 13:35:19 -0700

    Add fixes to output answer.  Quiz Skeleton complete.

--- js/scripts.js
+++ js/scripts.js
@@ -1,58 +1,99 @@
 $(document).ready(function() { /* This is the start of the wrapper to enable javascript to run at HTML document ready */
 
 /* The next section of code is to capture user input from the radio buttons on the index.html page. */
-  $("form#quiz-form-section").submit(function(event) {
+  $("#quiz-form-section").submit(function(event) {
     var answerQuestion1 = $("input:radio[name=quiz-q1]:checked").val();
     var answerQuestion2 = $("input:radio[name=quiz-q2]:checked").val();
     var answerQuestion3 = $("input:radio[name=quiz-q3]:checked").val();
     var answerQuestion4 = $("input:radio[name=quiz-q4]:checked").val();
     var answerQuestion5 = $("input:radio[name=quiz-q5]:checked").val();
 
-    alert(answerQuestion1, answerQuestion2, answerQuestion3, answerQuestion4, answerQuestion5);  /*debug line.....*/
+  /*Reset score keeping for final answer*/
 
-      var winnerAnswer1 = 0;
-      var winnerAnswer2 = 0;
-      var winnerAnswer3 = 0;
-      var winnerAnswer4 = 0;
-      var winnerAnswer5 = 0;
+    var winnerAnswer1 = 0;
+    var winnerAnswer2 = 0;
+    var winnerAnswer3 = 0;
+    var winnerAnswer4 = 0;
+    var winnerAnswer5 = 0;
 
-    /* The next section of code is to determine who is the "winner" of the quiz. This is done without looping or arrays.  Ties will count.*/
+  /* The next section of code sums the "points" of the answers. This is done without looping or arrays.*/
 
-      if (answerQuestion1 === "answer1") {winnerAnswer1 += 1}
-      else if (answerQuestion1 === "answer2") {winnerAnswer2 += 1}
-      else if (answerQuestion1 === "answer3") {winnerAnswer3 += 1}
-      else if (answerQuestion1 === "answer4") {winnerAnswer4 += 1}
-                                         else {winnerAnswer5 += 1}
+    if (answerQuestion1 === "answer1") {winnerAnswer1 += 1}
+    else if (answerQuestion1 === "answer2") {winnerAnswer2 += 1}
+    else if (answerQuestion1 === "answer3") {winnerAnswer3 += 1}
+    else if (answerQuestion1 === "answer4") {winnerAnswer4 += 1}
+    else {winnerAnswer5 += 1}
 
-      if (answerQuestion2 === "answer1") {winnerAnswer1 += 1}
-      else if (answerQuestion2 === "answer2") {winnerAnswer2 += 1}
-      else if (answerQuestion2 === "answer3") {winnerAnswer3 += 1}
-      else if (answerQuestion2 === "answer4") {winnerAnswer4 += 1}
-      else {winnerAnswer5 += 1}
+    if (answerQuestion2 === "answer1") {winnerAnswer1 += 1}
+    else if (answerQuestion2 === "answer2") {winnerAnswer2 += 1}
+    else if (answerQuestion2 === "answer3") {winnerAnswer3 += 1}
+    else if (answerQuestion2 === "answer4") {winnerAnswer4 += 1}
+    else {winnerAnswer5 += 1}
 
-      if (answerQuestion3 === "answer1") {winnerAnswer1 += 1}
-      else if (answerQuestion3 === "answer2") {winnerAnswer2 += 1}
-      else if (answerQuestion3 === "answer3") {winnerAnswer3 += 1}
-      else if (answerQuestion3 === "answer4") {winnerAnswer4 += 1}
-      else {winnerAnswer5 += 1}
+    if (answerQuestion3 === "answer1") {winnerAnswer1 += 1}
+    else if (answerQuestion3 === "answer2") {winnerAnswer2 += 1}
+    else if (answerQuestion3 === "answer3") {winnerAnswer3 += 1}
+    else if (answerQuestion3 === "answer4") {winnerAnswer4 += 1}
+    else {winnerAnswer5 += 1}
 
-      if (answerQuestion4 === "answer1") {winnerAnswer1 += 1}
-      else if (answerQuestion4 === "answer2") {winnerAnswer2 += 1}
-      else if (answerQuestion4 === "answer3") {winnerAnswer3 += 1}
-      else if (answerQuestion4 === "answer4") {winnerAnswer4 += 1}
-      else {winnerAnswer5 += 1}
+    if (answerQuestion4 === "answer1") {winnerAnswer1 += 1}
+    else if (answerQuestion4 === "answer2") {winnerAnswer2 += 1}
+    else if (answerQuestion4 === "answer3") {winnerAnswer3 += 1}
+    else if (answerQuestion4 === "answer4") {winnerAnswer4 += 1}
+    else {winnerAnswer5 += 1}
 
-      if (answerQuestion5 === "answer1") {winnerAnswer1 += 1}
-      else if (answerQuestion5 === "answer2") {winnerAnswer2 += 1}
-      else if (answerQuestion5 === "answer3") {winnerAnswer3 += 1}
-      else if (answerQuestion5 === "answer4") {winnerAnswer4 += 1}
-      else {winnerAnswer5 += 1}
+    if (answerQuestion5 === "answer1") {winnerAnswer1 += 1}
+    else if (answerQuestion5 === "answer2") {winnerAnswer2 += 1}
+    else if (answerQuestion5 === "answer3") {winnerAnswer3 += 1}
+    else if (answerQuestion5 === "answer4") {winnerAnswer4 += 1}
+    else {winnerAnswer5 += 1}
 
-    alert(answerQuestion1, answerQuestion2, answerQuestion3, answerQuestion4, answerQuestion5);  /*debug line.....*/
+    // alert(winnerAnswer1);  /*debug line.....*/
+    // alert(winnerAnswer2);  /*debug line.....*/
+    // alert(winnerAnswer3);  /*debug line.....*/
+    // alert(winnerAnswer4);  /*debug line.....*/
+    // alert(winnerAnswer5);  /*debug line.....*/
 
+    /*This next block of code determines which answer is the winner.  This is done without looping or arrays.  Note there is not a way to resolve ties.*/
 
+    if (winnerAnswer1 > winnerAnswer2 + winnerAnswer3 + winnerAnswer4 + winnerAnswer5)
+    {
+      $("#answer-final-a1").show();
+      $("#answer-final-a2").hide();
+      $("#answer-final-a3").hide();
+      $("#answer-final-a4").hide();
+      $("#answer-final-a5").hide();
+    } else if (winnerAnswer2 > winnerAnswer1 + winnerAnswer3 + winnerAnswer4 + winnerAnswer5)
+    {
+      $("#answer-final-a1").hide();
+      $("#answer-final-a2").show();
+      $("#answer-final-a3").hide();
+      $("#answer-final-a4").hide();
+      $("#answer-final-a5").hide();
+    } else if (winnerAnswer3 > winnerAnswer1 + winnerAnswer2 + winnerAnswer4 + winnerAnswer5)
+    {
+      $("#answer-final-a1").hide();
+      $("#answer-final-a2").hide();
+      $("#answer-final-a3").show();
+      $("#answer-final-a4").hide();
+      $("#answer-final-a5").hide();
+    } else if (winnerAnswer4 > winnerAnswer1 + winnerAnswer2 + winnerAnswer3 + winnerAnswer5)
+    {
+      $("#answer-final-a1").hidw();
+      $("#answer-final-a2").hide();
+      $("#answer-final-a3").hide();
+      $("#answer-final-a4").show();
+      $("#answer-final-a5").hide();
+    } else
+    {
+      $("#answer-final-a1").hide();
+      $("#answer-final-a2").hide();
+      $("#answer-final-a3").hide();
+      $("#answer-final-a4").hide();
+      $("#answer-final-a5").show();
+    }
 
-
+    event.preventDefault();
   });  /* This closes the form button submit logic */
 
 });  /* This is the end of the wrapper to enable javascript to run at HTML document ready */
